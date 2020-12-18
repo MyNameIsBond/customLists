a4ef7e0cbe08b20ae0e84feed08822d7ab2da4c9
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2020-12-18 22:27:20 +0000

    Initial Commit

---
+++ customList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  customList
+//
+//  Created by Tony Hajdini on 18/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ customList/customListApp.swift
@@ -0,0 +1,17 @@
+//
+//  customListApp.swift
+//  customList
+//
+//  Created by Tony Hajdini on 18/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct customListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
