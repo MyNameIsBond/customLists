44cfef0bdf06fe204de04901fa19391946a1ffd0
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2020-12-21 22:57:20 +0000

    first commit

--- customList/ContentView.swift
+++ customList/ContentView.swift
@@ -1,16 +1,54 @@
-//
-//  ContentView.swift
-//  customList
-//
-//  Created by Tony Hajdini on 18/12/2020.
-//
 
 import SwiftUI
 
+struct ListData: Identifiable {
+    var id = UUID()
+    var title: String
+    var postType: Array<String>
+    var date: String
+    var Image: String
+    var Color: Color
+}
+
+
+ 
+
+
+var data = [
+    ListData(title: "LazyHGrid in SwiftUI", postType: ["iOS","SwiftUI", "Xcode"], date: "15 dec", Image: "LazyGrid",Color: Color.red),
+    ListData(title: "LazyHGrid and LazyVGrid in SwiftUI ", postType: ["iOS","SwiftUI", "Xcode"], date: "15 dec", Image: "LazyGrid2",Color: Color.red),
+    ListData(title: "How to Detect Light/Dark theme", postType: ["iOS","SwiftUI", "Xcode"], date: "15 dec", Image: "themeDetector",Color: Color.green),
+    ListData(title: "Gradient Color", postType: ["iOS","SwiftUI", "Xcode"], date: "15 dec", Image: "try1",Color: Color.red)
+]
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(data) { m in
+                VStack {
+                    NavigationLink(destination: Text(m.title)) {
+                        HStack{
+                            Image(m.Image)
+                                .resizable()
+                                .frame(width: 120, height: 150)
+                            VStack {
+                                ForEach(m.postType) { e in
+                                    Text(e)
+                                        .foregroundColor(m.Color)
+                                        .font(.caption2)
+                                        .padding(5)
+                                        .overlay(
+                                           RoundedRectangle(cornerRadius: 10)
+                                            .stroke(m.Color, lineWidth: 1)
+                                       )
+                                }
+                            }
+                        }
+                    }.frame(height: 170)
+                }
+            }.listStyle(SidebarListStyle())
+            .navigationTitle("Post List")
+        }
     }
 }
 
